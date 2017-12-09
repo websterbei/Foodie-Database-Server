3017968136d9530517fd65912b50d188092f879e
websterbei <bei.yijie.2014@vjc.sg>
2017-12-09 03:25:48 -0500

    query

--- routing/topChoice.js
+++ routing/topChoice.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var Restaurant = require('/database/models/Restaurant').Restaurant;
+var Restaurant = require('../database/models/Restaurant').Restaurant;
 var router = express.Router();
 
 router.get('/:city', function(req, res) {
