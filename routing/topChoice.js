17dce83cb818f058304c50c52e8cb50470f7047d
websterbei <bei.yijie.2014@vjc.sg>
2017-12-09 03:33:11 -0500

    query

--- routing/topChoice.js
+++ routing/topChoice.js
@@ -6,10 +6,10 @@ router.get('/:city', function(req, res) {
   var city = req.params.city;
   var start = req.body.start; // starting index of the restaurant
   var end = req.body.end; // ending index of the restaurant
-  console.log(start);
-  console.log(end);
   if(!start) start = "1";
   if(!end) end = "5";
+  console.log(start);
+  console.log(end);
   Restaurant.find({_id:{$gte: start, $lte: end}}, function(err, restaurants) {
     if(err) res.send('Fail');
     else res.send(restaurants);
