da343657d5809e1196d41329485af21fc7ece7ff
websterbei <bei.yijie.2014@vjc.sg>
2017-12-09 03:48:15 -0500

    query

--- database/models/Restaurant.js
+++ database/models/Restaurant.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var restaurantSchema = new Schema({
-  _id: String,
+  _id: Number,
   name:  String,
   address: String,
   category:   String,
--- routing/topChoice.js
+++ routing/topChoice.js
@@ -6,10 +6,8 @@ router.get('/:city', function(req, res) {
   var city = req.params.city;
   var start = req.body.start; // starting index of the restaurant
   var end = req.body.end; // ending index of the restaurant
-  if(!start) start = "1";
-  if(!end) end = "5";
-  console.log(start);
-  console.log(end);
+  if(!start) start = 1;
+  if(!end) end = 5;
   Restaurant.find({_id:{$gte: start, $lte: end}}, function(err, restaurants) {
     if(err) res.send('Fail');
     else res.send(restaurants);
