440627bf9a2ce072731d1bf022a1bf97b92fa42e
websterbei <bei.yijie.2014@vjc.sg>
2017-12-21 15:50:52 +0800

    upload data

--- routing/saveToDatabase.js
+++ routing/saveToDatabase.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res) {
   var city = req.params.city;
   var newEntry = new Restaurant(req.body);
-  newEntry.save(function(err) => {
+  newEntry.save(function(err) {
     if(err) res.send('Failed');
     else res.send('Success');
   });
