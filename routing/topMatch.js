e89db762c7aced970d9ab744bc69e455df49e7a0
websterbei <bei.yijie.2014@vjc.sg>
2017-12-24 17:20:28 +0800

    latest

--- routing/topMatch.js
+++ routing/topMatch.js
@@ -1,19 +1,45 @@
 var express = require('express');
 var Restaurant = require('../database/models/Restaurant').Restaurant;
 var router = express.Router();
+var getUserPreference = require('./userData/getUserPreference');
 
+function computeScoreFactory(preference) {
+  function computeScore(restaurant) {
+    let basescore = preference.service * restaurant.service + preference.taste * restaurant.taste + preference.env * restaurant.env;
+    if(restaurant.category in preference.category) basescore *= 1.2;
+    return basescore;
+  }
+  return computeScore;
+}
 router.get('/:city', function(req, res) {
+  //Obtain post data
   var city = req.params.city;
   var longitude = req.body.longitude;
   var latitude = req.body.latitude;
   var user = req.body.user;
+  //Obtain user preference data
+  var preference = getUserPreference(user);
+  //Limit the number of restaurants returned
   var limit = 5;
+  //Indexed search on 2d sphere
   Restaurant.ensureIndexes({'location':'2dsphere'});
-  //Restaurant.ensureIndex({'location':'2dsphere'})
-  Restaurant.find( {city: city, location:{$near: [longitude, latitude], $maxDistance:10}}).limit(limit).exec(function(err, restaurants) {
-    console.log(limit);
+  //compute score function
+  var computeScore = computeScoreFactory(preference);
+  //Build query string
+  var queryString = {};
+  queryString.city = city;
+  queryString.location = {$near: [longitude, latitude], $maxDistance: preference.distance/111};
+  queryString.price = {$lt: preference.price*1.15}; //Accept 15% over price
+
+  Restaurant.find(queryString).limit(limit).exec(function(err, restaurants) {
     if(err) res.send('Fail');
-    else res.send(restaurants);
+    else {
+      restaurants.forEach(function(restaurant) {
+        restaurant.score = computeScore(restaurant);
+      });
+      restaurants.sort((restaurantA, restaurantB) => {return restaurantB.score - restaurantA.score;});
+      res.send(restaurants);
+    }
   });
 });
 
--- routing/userData/userPreference.js
+++ routing/userData/userPreference.js
@@ -1,5 +1,3 @@
-
-
 /*
   return
     preference for restaurant categories
@@ -7,7 +5,17 @@
     importance of taste from 1 to 10
     importance of environment from 1 to 10
     preferred price in RMB
-    
+    preferred distance within
 */
 function getUserPreference(user) {
+  return {
+    category: ['日本菜','北京菜'],
+    service: 8,
+    taste: 10,
+    env:  5,
+    price: 150,
+    distance: 2
+  }
 }
+
+module.exports = getUserPreference;
